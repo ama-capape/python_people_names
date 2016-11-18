6d35f0c2fd266f9b14b07fd61efa1090014a7e49
lindsay <techells22@gmail.com>
2016-11-18 08:25:00 -0600

    try fix encoding issues when importing package

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-# coding=utf-8
 
-# from distutils.core import setup
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
-# from codecs import open
-# from os import path
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 
 setup(name='python_people_names',
       version='0.0.1',
