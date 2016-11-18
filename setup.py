e7db2e3347a62ec0e7885007c02e80c1344faa0c
lindsay <techells22@gmail.com>
2016-11-18 10:37:53 -0600

    update setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
-# from distutils.core import setup
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-# from codecs import open
-# from os import path
 
 setup(name='python_people_names',
       version='0.0.1',
       description='people\'s name parser',
       author='lynzt',
-      url='https://github.com/lynzt/python_people_names'
+      url='https://github.com/lynzt/python_people_names',
+      packages = find_packages(),
      )
