b56802941d3d6551273865ec8c164ae6ec695924
lindsay <techells22@gmail.com>
2016-11-18 08:28:52 -0600

    try again encode issues

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
-
+# from distutils.core import setup
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
+# from codecs import open
+# from os import path
 
 setup(name='python_people_names',
       version='0.0.1',
