6fdcfd30023cc93162e16e70b71be9596c2b3e38
lindsay <techells22@gmail.com>
2016-11-18 08:22:15 -0600

    utf encoding?

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+# coding=utf-8
 
 # from distutils.core import setup
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
-from codecs import open
-from os import path
+# from codecs import open
+# from os import path
 
 setup(name='python_people_names',
       version='0.0.1',
