970e6e127a0bc25c066dba83b039bb653ae6cb0a
lindsay <techells22@gmail.com>
2016-11-27 12:00:52 -0600

    bump version and remove print from setup

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
-print find_packages()
 setup(name='python_people_names',
-      version='0.0.1',
+      version='0.0.2',
       description='people\'s name parser',
       author='lynzt',
       url='https://github.com/lynzt/python_people_names',
