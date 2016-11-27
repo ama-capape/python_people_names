abe297913d098521e5dbb452594fd906ea7c0f17
lindsay <techells22@gmail.com>
2016-11-27 11:46:55 -0600

    update script to use fml and lfm format

--- people_names/people_names.py
+++ people_names/people_names.py
@@ -1,27 +1,54 @@
-def split_name(name_str):
-    names = {}
+import re
+# name_format:
+# fml  ==> first middle last
+# lmf  ==> last middle first
+
+def split_name(name_str, name_format):
     if name_str == '':
         return {'first_name': '', 'middle_name': '', 'last_name': '', 'suffix_name': ''}
 
-    # special_name = special_names_override(name_str)
-    # if special_name:
-    #     return special_name
+    name_str = name_str.title() # convert to upper/lowercase
+    if name_format == 'lmf':
+        return process_last_middle_first(name_str)
+    elif name_format == 'fml':
+        return process_first_middle_last(name_str)
 
+
+def process_first_middle_last(name_str):
+    # print 'process_first_middle_last: %s' % name_str
+    names = {}
     name_arr = name_str.split(" ")
-    suffix_name = _check_suffix_name(name_arr)
-    if suffix_name != '':
-        name_arr = name_arr[:-1]
-        name_arr[-1] = str.rstrip(name_arr[-1], ',')
-        # name_arr[-1] = utils.strip_trailing_char(name_arr[-1], ',')
+    suffix_name = get_suffix_name(name_arr)
 
     names = _determine_name_if_last_name_has_prefix(name_arr)
+
     if not names:
         last_name = name_arr[-1]
-        first_name = _determine_first_name(name_arr[:-1])
-        middle_name = _determine_middle_name(name_arr[1:-1])
+        first_name = _get_first_element(name_arr[:-1])
+        middle_name = _get_join_elements(name_arr[1:-1])
         names = {'first_name': first_name, 'middle_name': middle_name, 'last_name': last_name}
-
     names['suffix_name'] = suffix_name
+
+    return names
+
+def process_last_middle_first(name_str):
+    # print 'process_last_middle_first: %s' + name_str
+    names = {}
+    name_arr = name_str.split(", ")
+    names['last_name'] = name_arr[0]
+    name_arr = (name_arr[1]).split(" ")
+    name_arr = _check_and_remove_nominal_and_nickname(name_arr)
+    names['suffix_name'] = get_suffix_name(name_arr)
+
+    names['first_name'] = _get_first_element(name_arr)
+    names['middle_name'] = _get_join_elements(name_arr[1:]) # don't pass first element (first_name)
+
+    ## done if suffix_name in last name vs end of first/middle
+    if names['suffix_name'] == '':
+        name_arr = (names['last_name']).split(" ")
+        names['suffix_name'] = get_suffix_name(name_arr)
+        names['last_name'] = _get_join_elements(name_arr)
+
     return names
 
 def add_name_parts_to_dict(obj, name_parts):
@@ -42,50 +69,96 @@ def _determine_name_if_last_name_has_prefix(name_arr):
     return names
 
 def _check_3_or_more_last_name(name_arr):
-    if name_arr[-3] == 'van' and name_arr[-2] == 'der':
-        first_name = _determine_first_name(name_arr[:-3])
-        middle_name = _determine_middle_name(name_arr[1:-3])
+    if name_arr[-3] == 'Van' and name_arr[-2] == 'Der':
+        first_name = _get_first_element(name_arr[:-3])
+        middle_name = _get_join_elements(name_arr[1:-3])
         last_name = name_arr[-3] + ' ' + name_arr[-2] + ' ' + name_arr[-1]
         return {'first_name': first_name, 'middle_name': middle_name, 'last_name': last_name}
     return None
 
 def _check_2_or_more_last_name(name_arr):
-    if name_arr[-2] == 'van':
+    if name_arr[-2] == 'Van':
         last_name = name_arr[-2] + ' ' + name_arr[-1]
-        first_name = _determine_first_name(name_arr[:-2])
-        middle_name = _determine_middle_name(name_arr[1:-2])
+        first_name = _get_first_element(name_arr[:-2])
+        middle_name = _get_join_elements(name_arr[1:-2])
         return {'first_name': first_name, 'middle_name': middle_name, 'last_name': last_name}
-    elif name_arr[-2] == 'von':
-        first_name = _determine_first_name(name_arr[:-2])
-        middle_name = _determine_middle_name(name_arr[1:-2])
+    elif name_arr[-2] == 'Von':
+        first_name = _get_first_element(name_arr[:-2])
+        middle_name = _get_join_elements(name_arr[1:-2])
         last_name = name_arr[-2] + ' ' + name_arr[-1]
         return {'first_name': first_name, 'middle_name': middle_name, 'last_name': last_name}
     return None
 
-def _determine_first_name(names):
+def _get_first_element(names):
+    if len(names) == 0:
+        return ''
     return names[0]
 
 
-def _determine_middle_name(names):
+def _get_join_elements(names):
     if len(names) == 0:
         return ''
     if len(names) == 1:
         return names[0]
     else:
-        return names.join(' ')
+        return ' '.join(names)
 
-def _check_suffix_name(names):
-    if names[-1] == 'Jr.':
-        return 'Jr.'
-    elif names[-1] == 'Sr.':
-        return 'Sr.'
-    elif names[-1] == 'II':
-        return 'II'
-    elif names[-1] == 'III':
-        return 'III'
-    elif names[-1] == 'IV':
-        return 'IV'
-    elif names[-1] == 'V':
-        return 'V'
+def _check_and_remove_nominal_and_nickname(names):
+    names_no_post_nominal = []
+    for name in names:
+        if not _check_post_nominal(name) and not _check_nickname(name):
+            names_no_post_nominal.append(name)
+
+    return names_no_post_nominal
+
+
+def _check_nickname(name):
+    nickname = False
+    if re.match(r'^\(.*\)$', name, re.IGNORECASE):
+        nickname = True
+    return nickname
+
+def _check_post_nominal(name):
+    post_nominal = False
+    if re.match(r'^Ph(.?)D(.?)$', name, re.IGNORECASE):
+        post_nominal = True
+    elif re.match(r'^Dr(.?)$', name, re.IGNORECASE):
+        post_nominal = True
+    elif re.match(r'^MD(.?)$', name, re.IGNORECASE):
+        post_nominal = True
+    elif re.match(r'^Mr(.?)$', name, re.IGNORECASE):
+        post_nominal = True
+    elif re.match(r'^MS.(.?)$', name, re.IGNORECASE):
+        post_nominal = True
+    elif re.match(r'^Rev(.?)$', name, re.IGNORECASE):
+        post_nominal = True
+    elif re.match(r'^dvm$', name, re.IGNORECASE):
+        post_nominal = True
+    elif re.match(r'^the$', name, re.IGNORECASE):
+        post_nominal = True
+    return post_nominal
+
+def get_suffix_name(names):
+    last_element = names[-1]
+    if re.match(r'^Jr(.?)$', last_element, re.IGNORECASE):
+        del names[-1]
+        suffix_name = 'Jr.'
+    elif re.match(r'^Sr(.?)$', last_element, re.IGNORECASE):
+        del names[-1]
+        suffix_name = 'Sr.'
+    elif re.match(r'^II$', last_element, re.IGNORECASE):
+        del names[-1]
+        suffix_name = 'II'
+    elif re.match(r'^III$', last_element, re.IGNORECASE):
+        del names[-1]
+        suffix_name = 'III'
+    elif re.match(r'^IV$', last_element, re.IGNORECASE):
+        del names[-1]
+        suffix_name = 'IV'
+    elif re.match(r'^V$', last_element, re.IGNORECASE):
+        del names[-1]
+        suffix_name = 'V'
     else:
-        return ''
+        suffix_name = ''
+
+    return suffix_name
--- tests/people_names_tests.py
+++ tests/people_names_tests.py
@@ -2,53 +2,116 @@ import unittest
 from people_names import people_names
 
 class UtilsTests(unittest.TestCase):
-    def test_split_name(self):
-        names = people_names.split_name('')
+    def test_split_name_fml(self):
+        names = people_names.split_name('', 'fml')
         self.assertEqual(names['first_name'], '')
         self.assertEqual(names['middle_name'], '')
         self.assertEqual(names['last_name'], '')
         self.assertEqual(names['suffix_name'], '')
 
-        names = people_names.split_name('Jacqueline Williams-Roll')
+        names = people_names.split_name('Jacqueline Williams-Roll', 'fml')
         self.assertEqual(names['first_name'], 'Jacqueline')
         self.assertEqual(names['middle_name'], '')
         self.assertEqual(names['last_name'], 'Williams-Roll')
         self.assertEqual(names['suffix_name'], '')
 
-        names = people_names.split_name('Alicia Boler Davis')
-        self.assertEqual(names['first_name'], 'Alicia')
-        self.assertEqual(names['middle_name'], 'Boler')
-        self.assertEqual(names['last_name'], 'Davis')
-        self.assertEqual(names['suffix_name'], '')
-
-        names = people_names.split_name('G. Zachary Gund')
-        self.assertEqual(names['first_name'], 'G.')
-        self.assertEqual(names['middle_name'], 'Zachary')
-        self.assertEqual(names['last_name'], 'Gund')
-        self.assertEqual(names['suffix_name'], '')
-
-        names = people_names.split_name('Archbold van Beuren')
+        names = people_names.split_name('Archbold van Beuren', 'fml')
         self.assertEqual(names['first_name'], 'Archbold')
         self.assertEqual(names['middle_name'], '')
-        self.assertEqual(names['last_name'], 'van Beuren')
+        self.assertEqual(names['last_name'], 'Van Beuren')
         self.assertEqual(names['suffix_name'], '')
 
-        names = people_names.split_name('Jan van der Velden')
+        names = people_names.split_name('Jan van der Velden', 'fml')
         self.assertEqual(names['first_name'], 'Jan')
         self.assertEqual(names['middle_name'], '')
-        self.assertEqual(names['last_name'], 'van der Velden')
+        self.assertEqual(names['last_name'], 'Van Der Velden')
         self.assertEqual(names['suffix_name'], '')
 
-        names = people_names.split_name('Carl-Henric Svanberg')
+        names = people_names.split_name('Carl-Henric Svanberg', 'fml')
         self.assertEqual(names['first_name'], 'Carl-Henric')
         self.assertEqual(names['middle_name'], '')
         self.assertEqual(names['last_name'], 'Svanberg')
         self.assertEqual(names['suffix_name'], '')
 
-        names = people_names.split_name('Harry F. Hixson Jr.')
+        names = people_names.split_name('Harry F. Hixson Jr.', 'fml')
         self.assertEqual(names['first_name'], 'Harry')
         self.assertEqual(names['middle_name'], 'F.')
         self.assertEqual(names['last_name'], 'Hixson')
         self.assertEqual(names['suffix_name'], 'Jr.')
+
+    def test_split_name_lmf(self):
+        names = people_names.split_name('', 'lfm')
+        self.assertEqual(names['first_name'], '')
+        self.assertEqual(names['middle_name'], '')
+        self.assertEqual(names['last_name'], '')
+        self.assertEqual(names['suffix_name'], '')
+
+        names = people_names.split_name('Davis, Alicia Boler', 'lmf')
+        self.assertEqual(names['first_name'], 'Alicia')
+        self.assertEqual(names['middle_name'], 'Boler')
+        self.assertEqual(names['last_name'], 'Davis')
+        self.assertEqual(names['suffix_name'], '')
+
+        names = people_names.split_name('ABDULHAMID, AMMAR', 'lmf')
+        self.assertEqual(names['first_name'], 'Ammar')
+        self.assertEqual(names['middle_name'], '')
+        self.assertEqual(names['last_name'], 'Abdulhamid')
+        self.assertEqual(names['suffix_name'], '')
+
+        names = people_names.split_name('ABBOTT, GIFFORD WHEELER JR', 'lmf')
+        self.assertEqual(names['first_name'], 'Gifford')
+        self.assertEqual(names['middle_name'], 'Wheeler')
+        self.assertEqual(names['last_name'], 'Abbott')
+        self.assertEqual(names['suffix_name'], 'Jr.')
+
+        names = people_names.split_name('ABRAMS, MARCIA KAREN DR PHD', 'lmf')
+        self.assertEqual(names['first_name'], 'Marcia')
+        self.assertEqual(names['middle_name'], 'Karen')
+        self.assertEqual(names['last_name'], 'Abrams')
+        self.assertEqual(names['suffix_name'], '')
+
+        names = people_names.split_name('ABRAHAM, RALPH LEE DR. JR', 'lmf')
+        self.assertEqual(names['first_name'], 'Ralph')
+        self.assertEqual(names['middle_name'], 'Lee')
+        self.assertEqual(names['last_name'], 'Abraham')
+        self.assertEqual(names['suffix_name'], 'Jr.')
+
+        names = people_names.split_name('ALARCON, RICHARD ANTHONY MR.', 'lmf')
+        self.assertEqual(names['first_name'], 'Richard')
+        self.assertEqual(names['middle_name'], 'Anthony')
+        self.assertEqual(names['last_name'], 'Alarcon')
+        self.assertEqual(names['suffix_name'], '')
+
+        names = people_names.split_name('ANNA, SANTA MR. THE III', 'lmf')
+        self.assertEqual(names['first_name'], 'Santa')
+        self.assertEqual(names['middle_name'], '')
+        self.assertEqual(names['last_name'], 'Anna')
+        self.assertEqual(names['suffix_name'], 'III')
+
+        names = people_names.split_name('ANTLEY, RAY MILLS JR MD', 'lmf')
+        self.assertEqual(names['first_name'], 'Ray')
+        self.assertEqual(names['middle_name'], 'Mills')
+        self.assertEqual(names['last_name'], 'Antley')
+        self.assertEqual(names['suffix_name'], 'Jr.')
+
+        names = people_names.split_name('BONIFICIA, ANTHONY A JR', 'lmf')
+        self.assertEqual(names['first_name'], 'Anthony')
+        self.assertEqual(names['middle_name'], 'A')
+        self.assertEqual(names['last_name'], 'Bonificia')
+        self.assertEqual(names['suffix_name'], 'Jr.')
+
+        names = people_names.split_name('VON ADAMS III, PHILIP MONROE', 'lmf')
+        self.assertEqual(names['first_name'], 'Philip')
+        self.assertEqual(names['middle_name'], 'Monroe')
+        self.assertEqual(names['last_name'], 'Von Adams')
+        self.assertEqual(names['suffix_name'], 'III')
+
+        names = people_names.split_name('BRASS JR, HERMAN (GEORGE)', 'lmf')
+        self.assertEqual(names['first_name'], 'Herman')
+        self.assertEqual(names['middle_name'], '')
+        self.assertEqual(names['last_name'], 'Brass')
+        self.assertEqual(names['suffix_name'], 'Jr.')
+
+
 
 # python -m unittest discover -s tests -p "*_tests.py"
