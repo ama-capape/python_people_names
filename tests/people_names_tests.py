96463a29bf0a7a310766c5ce389a96532cff7562
lindsay <techells22@gmail.com>
2016-11-28 19:20:48 -0600

    update script to account for esq

--- tests/people_names_tests.py
+++ tests/people_names_tests.py
@@ -83,6 +83,13 @@ class UtilsTests(unittest.TestCase):
         self.assertEqual(names['suffix_name'], '')
         self.assertEqual(names['nickname'], '')
 
+        names = people_names.split_name('Robert D. Cochran Esq.', 'fml')
+        self.assertEqual(names['first_name'], 'Robert')
+        self.assertEqual(names['middle_name'], 'D')
+        self.assertEqual(names['last_name'], 'Cochran')
+        self.assertEqual(names['suffix_name'], '')
+        self.assertEqual(names['nickname'], '')
+
     def test_split_name_lmf(self):
         names = people_names.split_name('', 'lfm')
         self.assertEqual(names['first_name'], '')
