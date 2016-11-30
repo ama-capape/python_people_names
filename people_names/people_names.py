b231e7549ad57e3264624f0ba7de902eccd2630c
lindsay <techells22@gmail.com>
2016-11-29 23:14:23 -0600

    updates to add: Lieutenant and Governor. also strip all periods first

--- people_names/people_names.py
+++ people_names/people_names.py
@@ -15,6 +15,11 @@ def split_name(name_str, name_format):
     if name_str == '':
         return {'first_name': '', 'middle_name': '', 'last_name': '', 'suffix_name': '', 'nominal_name': '', 'nickname': ''}
 
+    # print 'split_name: %s' % name_str
+    name_str = name_str.strip()
+    name_str = name_str.translate(None, '.')
+    # print name_str
+
     if name_format == 'lmf':
         return _process_last_middle_first(name_str)
     elif name_format == 'fml':
@@ -126,9 +131,10 @@ def _determine_set_name(name, name_new):
 
 def _check_for_nickname(name):
     nickname = _check_nickname(name)
-
     # override nicknames if not true nicknames
-    if re.match(r'^retd.$', nickname, re.IGNORECASE):
+    if re.match(r'^retd$', nickname, re.IGNORECASE):
+        nickname = ''
+    elif re.match(r'^Retired$', nickname, re.IGNORECASE):
         nickname = ''
     return nickname
 
@@ -142,72 +148,81 @@ def _check_nickname(name):
 
 def _check_post_nominal(name):
     post_nominal = ''
-    if re.match(r'^Mr(\.?)$', name, re.IGNORECASE):
+    if re.match(r'^Mr$', name, re.IGNORECASE):
         post_nominal = 'Mr'
-    elif re.match(r'^MS(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^MS$', name, re.IGNORECASE):
         post_nominal = 'Ms'
-    elif re.match(r'^Mrs(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Mrs$', name, re.IGNORECASE):
         post_nominal = 'Mrs'
-    elif re.match(r'^Ph(\.?)D(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^PhD$', name, re.IGNORECASE):
         post_nominal = 'PhD'
-    elif re.match(r'^Dr(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Dr$', name, re.IGNORECASE):
         post_nominal = 'Dr'
-    elif re.match(r'^M(\.?)D(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^MD$', name, re.IGNORECASE):
         post_nominal = 'MD'
-    elif re.match(r'^J(\.?)D(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^JD$', name, re.IGNORECASE):
         post_nominal = 'JD'
-    elif re.match(r'^Esq(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Esq$', name, re.IGNORECASE):
         post_nominal = 'Esq'
-    elif re.match(r'^CPA(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^CPA$', name, re.IGNORECASE):
         post_nominal = 'CPA'
-    elif re.match(r'^Gen(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Gen$', name, re.IGNORECASE):
         post_nominal = 'Gen'
-    elif re.match(r'^Gen(\.?)\s?\(Retd(\.?)\)(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^General$', name, re.IGNORECASE):
         post_nominal = 'Gen'
-    elif re.match(r'^\(Retd\.\)$', name, re.IGNORECASE):
+    elif re.match(r'^Gen\s?\(Retd\)$', name, re.IGNORECASE):
+        post_nominal = 'Gen'
+    elif re.match(r'^\(Retd\)$', name, re.IGNORECASE):
         post_nominal = 'Retd'
-    elif re.match(r'^Lt(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^\(Retired\)$', name, re.IGNORECASE):
+        post_nominal = 'Retd'
+    elif re.match(r'^Lt$', name, re.IGNORECASE):
         post_nominal = 'Lt'
-    elif re.match(r'^Sir(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Lieutenant$', name, re.IGNORECASE):
+        post_nominal = 'Lt'
+    elif re.match(r'^Sir$', name, re.IGNORECASE):
         post_nominal = 'Sir'
-    elif re.match(r'^CFA(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^CFA$', name, re.IGNORECASE):
         post_nominal = 'CFA'
-    elif re.match(r'^CBE(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^CBE$', name, re.IGNORECASE):
         post_nominal = 'CBE'
-    elif re.match(r'^Rev(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Rev$', name, re.IGNORECASE):
         post_nominal = 'Rev'
-    elif re.match(r'^Prof(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Prof$', name, re.IGNORECASE):
         post_nominal = 'Prof'
     elif re.match(r'^dvm$', name, re.IGNORECASE):
         post_nominal = 'Dmv'
     elif re.match(r'^the$', name, re.IGNORECASE):
         post_nominal = 'the'
-    elif re.match(r'^Hon(\.?)(\,?)$', name, re.IGNORECASE):
+    elif re.match(r'^Hon(\,?)$', name, re.IGNORECASE):
         post_nominal = 'Hon'
-    elif re.match(r'^Amb(\.?)(\,?)$', name, re.IGNORECASE):
+    elif re.match(r'^Amb(\,?)$', name, re.IGNORECASE):
         post_nominal = 'Amb'
-    elif re.match(r'^Gov(\.?)$', name, re.IGNORECASE):
+    elif re.match(r'^Gov$', name, re.IGNORECASE):
         post_nominal = 'Gov'
+    elif re.match(r'^Governor$', name, re.IGNORECASE):
+        post_nominal = 'Gov'
+
     return post_nominal
 
 def get_suffix_name(names):
     last_element = names[-1]
-    if re.match(r'^Jr(\.?)$', last_element, re.IGNORECASE):
+    if re.match(r'^Jr$', last_element, re.IGNORECASE):
         del names[-1]
         suffix_name = 'Jr'
-    elif re.match(r'^Sr(\.?)$', last_element, re.IGNORECASE):
+    elif re.match(r'^Sr$', last_element, re.IGNORECASE):
         del names[-1]
         suffix_name = 'Sr'
-    elif re.match(r'^II(\.?)$', last_element, re.IGNORECASE):
+    elif re.match(r'^II$', last_element, re.IGNORECASE):
         del names[-1]
         suffix_name = 'II'
-    elif re.match(r'^III(\.?)$', last_element, re.IGNORECASE):
+    elif re.match(r'^III$', last_element, re.IGNORECASE):
         del names[-1]
         suffix_name = 'III'
-    elif re.match(r'^IV(\.?)$', last_element, re.IGNORECASE):
+    elif re.match(r'^IV$', last_element, re.IGNORECASE):
         del names[-1]
         suffix_name = 'IV'
-    elif re.match(r'^V(\.?)$', last_element, re.IGNORECASE):
+    elif re.match(r'^V$', last_element, re.IGNORECASE):
         del names[-1]
         suffix_name = 'V'
     else:
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python_people_names',
-      version='0.0.12',
+      version='0.0.13',
       description='people\'s name parser',
       author='lynzt',
       url='https://github.com/lynzt/python_people_names',
