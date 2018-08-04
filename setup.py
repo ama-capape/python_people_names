4480c32b624a37777a1e9d7e06fbe710e50bdb76
lynzt <techells22@gmail.com>
2018-08-04 14:02:28 -0500

    add dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='python_people_names',
-      version='0.0.26',
-      description='people\'s name parser',
-      author='lynzt',
-      url='https://github.com/lynzt/python_people_names',
-      packages=['people_names']
-     )
+    version='0.0.27',
+    description='people\'s name parser',
+    author='lynzt',
+    url='https://github.com/lynzt/python_people_names',
+    packages=['people_names'],
+    install_requires=[
+        unidecode,
+        awesome-slugify
+    ]
+)
