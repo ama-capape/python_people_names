73767ac5afae7c7951f2ad6743e10f60518c0fcf
lynzt <techells22@gmail.com>
2018-10-24 21:33:17 -0500

    use reduce instead of loop for stakeholder positions

---
+++ people_names/stakeholder_positions.py
@@ -0,0 +1,12 @@
+def get():
+    return ['Vice Chairman and Chairman of Goldman Sachs Asia Pacific',
+    'Executive Vice President and Chief Financial Officer',
+    'Executive Vice President, General Counsel and Secretary of the Corporation',
+    'Executive Vice President and Head of Global Compliance',
+    'Executive Vice President, Chief of Staff and Secretary to the Board',
+    'Executive Vice President and Global Head of Human Capital Management',
+    'President and Chief Operating Officer',
+    'Chairman and Chief Executive Officer',
+    'President and COO',
+    'Lead Director',
+    'Vice Chairman']
